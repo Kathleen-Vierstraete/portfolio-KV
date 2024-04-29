39b4e8e227f9d49a325a0953cd65d1b7ce2efb70
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2024-04-29 11:21:49 +0200

    feat: modification image links on logos after vercel deploy

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -1,7 +1,17 @@
 import { HeroAvatar, HeroSocial, Section } from 'astro-boilerplate-components';
 
 import AvatarKV from '../../public/assets/images/AvatarMaker-removebg-preview.png';
+import ChinaFlag from '../../public/assets/images/languagesIcons/icons8-china-48.png';
+import CSSLogo from '../../public/assets/images/languagesIcons/icons8-css3-48.png';
+import FranceFlag from '../../public/assets/images/languagesIcons/icons8-france-48.png';
 import HTMLLogo from '../../public/assets/images/languagesIcons/icons8-html-48.png';
+import JSLogo from '../../public/assets/images/languagesIcons/icons8-javascript-48.png';
+import MySQLLogo from '../../public/assets/images/languagesIcons/icons8-mysql-48.png';
+import PHPLogo from '../../public/assets/images/languagesIcons/icons8-php-48.png';
+import ReactLogo from '../../public/assets/images/languagesIcons/icons8-react-48.png';
+import SpainFlag from '../../public/assets/images/languagesIcons/icons8-spain-flag-48.png';
+import SymfonyLogo from '../../public/assets/images/languagesIcons/icons8-symfony-48.png';
+import UkFlag from '../../public/assets/images/languagesIcons/icons8-uk-48.png';
 
 const Hero = () => (
   <Section>
@@ -30,35 +40,17 @@ const Hero = () => (
         <div className="mt-5 grid grid-cols-4 gap-4 md:grid-cols-8">
           <HeroSocial src={HTMLLogo.src} alt="HTML icon" />
 
-          <HeroSocial
-            src="../public/assets/images/languagesIcons/icons8-css3-48.png"
-            alt="CSS icon"
-          />
+          <HeroSocial src={CSSLogo.src} alt="CSS icon" />
 
-          <HeroSocial
-            src="../public/assets/images/languagesIcons/icons8-php-48.png"
-            alt="PHP icon"
-          />
+          <HeroSocial src={PHPLogo.src} alt="PHP icon" />
 
-          <HeroSocial
-            src="../public/assets/images/languagesIcons/icons8-javascript-48.png"
-            alt="JavaScript icon"
-          />
+          <HeroSocial src={JSLogo.src} alt="JavaScript icon" />
 
-          <HeroSocial
-            src="../public/assets/images/languagesIcons/icons8-symfony-48.png"
-            alt="Symfony icon"
-          />
+          <HeroSocial src={SymfonyLogo.src} alt="Symfony icon" />
 
-          <HeroSocial
-            src="../public/assets/images/languagesIcons/icons8-react-48.png"
-            alt="React icon"
-          />
+          <HeroSocial src={ReactLogo.src} alt="React icon" />
 
-          <HeroSocial
-            src="../public/assets/images/languagesIcons/icons8-mysql-48.png"
-            alt="MySQL icon"
-          />
+          <HeroSocial src={MySQLLogo.src} alt="MySQL icon" />
         </div>
       }
     />
@@ -69,34 +61,22 @@ const Hero = () => (
       socialButtons={
         <div className="grid grid-cols-4 gap-4 md:grid-cols-8">
           <div className="flex flex-col items-center">
-            <HeroSocial
-              src="../public/assets/images/languagesIcons/icons8-france-48.png"
-              alt="HTML icon"
-            />
+            <HeroSocial src={FranceFlag.src} alt="France Flag" />
             <p>C2</p>
           </div>
 
           <div className="flex flex-col items-center">
-            <HeroSocial
-              src="../public/assets/images/languagesIcons/icons8-uk-48.png"
-              alt="CSS icon"
-            />
+            <HeroSocial src={UkFlag.src} alt="UK Flag" />
             <p>C1</p>
           </div>
 
           <div className="flex flex-col items-center">
-            <HeroSocial
-              src="../public/assets/images/languagesIcons/icons8-spain-flag-48.png"
-              alt="PHP icon"
-            />
+            <HeroSocial src={SpainFlag.src} alt="Spain Flag" />
             <p>B1</p>
           </div>
 
           <div className="flex flex-col items-center">
-            <HeroSocial
-              src="../public/assets/images/languagesIcons/icons8-china-48.png"
-              alt="JavaScript icon"
-            />
+            <HeroSocial src={ChinaFlag.src} alt="China Flag" />
             <p>A2</p>
           </div>
         </div>
