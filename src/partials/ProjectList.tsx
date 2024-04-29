da478b982ff885513f706286ad62713c61d32a29
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2024-04-29 11:46:49 +0200

    feat: modification image links  projetlist + tab icon  after vercel deploy

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -13,7 +13,7 @@ const ProjectList = () => (
         description="Site de rencontre pour chiens, où les propriétaires de chiens peuvent se rencontrer et organiser des rendez-vous pour leurs compagnons à quatre pattes, pour des parties de jeux ou des promenades inoubliables. "
         link="opoils/"
         img={{
-          src: '../../public/assets/images/opoils-logo.png',
+          src: '/assets/images/opoils-logo.png',
           alt: 'Project Opoils Logo',
         }}
         category={
@@ -34,7 +34,7 @@ const ProjectList = () => (
         description="Site de vente d'articles multimédias. Envie d'acheter le casque dernier cri pour des parties endiablées en ligne? Ne cherchez pas plus loin."
         link="pixelperfect/"
         img={{
-          src: '../../public/assets/images/pp-logo2.png',
+          src: '/assets/images/pp-logo2.png',
           alt: 'Project Pixel Perfect',
         }}
         category={
@@ -53,7 +53,7 @@ const ProjectList = () => (
         description="Une application pour s'organiser, en version multilingue."
         link="react-todo-list/"
         img={{
-          src: '../../public/assets/images/todo-list-logo2.png',
+          src: '/assets/images/todo-list-logo2.png',
           alt: 'Project Todo list logo',
         }}
         category={
@@ -71,7 +71,7 @@ const ProjectList = () => (
         description="Une petite pause via ce jeu pour m'aider à trouver de nouvelles opportunités et redévouvrir mon CV."
         link="tinygame/"
         img={{
-          src: '../../public/assets/images/laby-logo2.png',
+          src: '/assets/images/laby-logo2.png',
           alt: 'Project Todo list logo',
         }}
         category={
