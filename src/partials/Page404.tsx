fc24ef49919223c6a1cda3da286f6f6682461490
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2024-05-07 16:24:12 +0200

    feat: adding a 404 page from a template

---
+++ src/partials/Page404.tsx
@@ -0,0 +1,14 @@
+const Page404 = () => (
+  <div id="notfound">
+    <div className="notfound">
+      <div className="notfound-404">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+      </div>
+      <a href="/">Homepage</a>
+    </div>
+  </div>
+  // <!-- This templates was made by Colorlib (https://colorlib.com) -->
+);
+
+export { Page404 };
