e5b855dadbd7b1fdf6f22880517319ae5ef45bf9
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2024-06-25 15:47:48 +0200

    feat: creating the english index, starting translation of heroenglish

---
+++ src/partials/HeroEnglish.tsx
@@ -0,0 +1,88 @@
+import { HeroAvatar, HeroSocial, Section } from 'astro-boilerplate-components';
+
+import AvatarKV from '../../public/assets/images/AvatarMaker-removebg-preview.png';
+import ChinaFlag from '../../public/assets/images/languagesIcons/icons8-china-48.png';
+import CSSLogo from '../../public/assets/images/languagesIcons/icons8-css3-48.png';
+import FranceFlag from '../../public/assets/images/languagesIcons/icons8-france-48.png';
+import HTMLLogo from '../../public/assets/images/languagesIcons/icons8-html-48.png';
+import JSLogo from '../../public/assets/images/languagesIcons/icons8-javascript-48.png';
+import MySQLLogo from '../../public/assets/images/languagesIcons/icons8-mysql-48.png';
+import PHPLogo from '../../public/assets/images/languagesIcons/icons8-php-48.png';
+import ReactLogo from '../../public/assets/images/languagesIcons/icons8-react-48.png';
+import SpainFlag from '../../public/assets/images/languagesIcons/icons8-spain-flag-48.png';
+import SymfonyLogo from '../../public/assets/images/languagesIcons/icons8-symfony-48.png';
+import UkFlag from '../../public/assets/images/languagesIcons/icons8-uk-48.png';
+
+const HeroEnglish = () => (
+  <Section>
+    <HeroAvatar
+      title={<>Hello, my name is Kate</>}
+      description={
+        <>
+          I am a Fullstack developer, with a preference for the BackEnd. Feel
+          free to discover my projects, developped remotely on my own or with
+          teammates. I am available for new adventures full of code and
+          challenges. Contact :{' '}
+          <span className="text-purple-800 hover:underline">
+            <a href="mailto:ctrlkate89@gmail.com">ctrlkate89@gmail.com</a>
+          </span>
+        </>
+      }
+      avatar={
+        <img
+          className="h-80 w-80"
+          src={AvatarKV.src}
+          alt="Avatar image"
+          loading="lazy"
+        />
+      }
+      socialButtons={
+        <div className="mt-5 grid grid-cols-4 gap-4 md:grid-cols-8">
+          <HeroSocial src={HTMLLogo.src} alt="HTML icon" />
+
+          <HeroSocial src={CSSLogo.src} alt="CSS icon" />
+
+          <HeroSocial src={PHPLogo.src} alt="PHP icon" />
+
+          <HeroSocial src={JSLogo.src} alt="JavaScript icon" />
+
+          <HeroSocial src={SymfonyLogo.src} alt="Symfony icon" />
+
+          <HeroSocial src={ReactLogo.src} alt="React icon" />
+
+          <HeroSocial src={MySQLLogo.src} alt="MySQL icon" />
+        </div>
+      }
+    />
+    <HeroAvatar
+      title={<></>}
+      description={<>I speak multiple languages</>}
+      avatar={<></>}
+      socialButtons={
+        <div className="grid grid-cols-4 gap-4 md:grid-cols-8">
+          <div className="flex flex-col items-center">
+            <HeroSocial src={FranceFlag.src} alt="France Flag" />
+            <p>C2</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <HeroSocial src={UkFlag.src} alt="UK Flag" />
+            <p>C1</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <HeroSocial src={SpainFlag.src} alt="Spain Flag" />
+            <p>B1</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <HeroSocial src={ChinaFlag.src} alt="China Flag" />
+            <p>A2</p>
+          </div>
+        </div>
+      }
+    />
+  </Section>
+);
+
+export { HeroEnglish };
